fa6e6e163e2b7b28d30216c6c88ac4f25c96c942
nurul <nurulsyahira94@gmail.com>
2017-10-30 09:09:51 +0800

    Initial commit

---
+++ app/src/main/java/myapplication/joji/firebasedatabasecrud/Artist.java
@@ -0,0 +1,28 @@
+package myapplication.joji.firebasedatabasecrud;
+
+/**
+ * Created by nurul on 30/10/2017.
+ */
+
+public class Artist {
+
+    String artistId;
+    String artistName;
+
+    public Artist(){}
+
+    public Artist(String artistId, String artistName) {
+        this.artistId = artistId;
+        this.artistName = artistName;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+
+}
